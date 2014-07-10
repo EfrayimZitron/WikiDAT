b55105020ef77146edf87b92dd640620b5a40a54
Efrayim Zitron <efrayim.zitron@nyu.edu>
2014-07-10 14:20:47 -0400

    Added new script for MySQLdb + SQLAlchemy Core

---
+++ wikidat/sources/db/SQLAlchemy_files/testload_mysqldb_core.py
@@ -0,0 +1,58 @@
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import MySQLdb
+import csv
+import sys
+import dbschema_core
+import timeit
+
+engine = create_engine('mysql+mysqldb://root:@localhost/?charset=utf8')
+engine.execute("USE wikidb")
+
+with open('page.csv') as page_file:
+    fieldnames = ('page_id', 'page_namepsace', 'page_title', 'page_restrictions')
+    reader = csv.DictReader(page_file, fieldnames = fieldnames, delimiter='\t')
+    page_dict = [row for row in reader]
+
+with open('revision.csv') as revision_file:
+    fieldnames = ('rev_id', 'rev_page', 'rev_user', 'rev_timestamp', 'rev_len', 'rev_parent_id',
+                  'rev_is_redirect', 'rev_minor_edit', 'rev_fa', 'rev_flist', 'rev_ga', 'rev_comment')
+    reader = csv.DictReader(revision_file, fieldnames = fieldnames, delimiter='\t')
+    revision_dict = []
+    for row in reader:
+        if row['rev_parent_id'] == 'NULL':
+            row['rev_parent_id'] = None 
+        revision_dict.append(row)
+    
+
+with open('revision_hash.csv') as revision_hash_file:
+    fieldnames = ('rev_id', 'rev_page', 'rev_user', 'rev_hash')
+    reader = csv.DictReader(revision_hash_file, fieldnames = fieldnames, delimiter='\t')
+    revision_hash_dict = [row for row in reader]
+
+
+def load():
+    engine.execute(dbschema_core.page.insert(),page_dict)
+
+    engine.execute(dbschema_core.revision.insert(),revision_dict)
+
+    engine.execute(dbschema_core.revision_hash.insert(),revision_hash_dict) 
+
+# Timer calculates how much time it takes for the data to be loaded into the database.
+# Had to specify that the timer should only run one execution of the code since multiple runs of inserting identical
+# data would fail due to primary key violations
+timer = timeit.Timer(stmt='load()', setup='from __main__ import load')
+
+total = []
+
+def run_time_test():
+    dbschema_core.metadata.drop_all(engine)
+    dbschema_core.metadata.create_all(engine)   
+    return(timer.timeit(number=1))
+    
+for x in range(int(sys.argv[1])):
+    total.append(run_time_test())
+    print 'Run', x, 'took', total[x]
+    
+print 'Average running time:', sum(total)/len(total)
