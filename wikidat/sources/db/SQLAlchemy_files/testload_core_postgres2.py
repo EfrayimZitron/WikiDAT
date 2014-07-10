5fd524d18c7157237fdc2f7a770e8be57a20f0d6
Efrayim Zitron <efrayim.zitron@nyu.edu>
2014-07-10 12:06:12 -0400

    Made separate Postgres-insert scripts fro Py 2 and Py 3

--- wikidat/sources/db/SQLAlchemy_files/testload_core_postgres2.py
+++ wikidat/sources/db/SQLAlchemy_files/testload_core_postgres2.py
@@ -1,7 +1,6 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-#import statistics        #Py 3
 import psycopg2
 import csv
 import sys
@@ -12,14 +11,12 @@ import timeit
 engine = create_engine('postgresql+psycopg2://postgres:root@localhost/wikidb')
 
 with open('page.csv', "rb") as page_file:
-#with open('page.csv', newline = '' , encoding = 'utf-8') as page_file:			#Py 3
     fieldnames = ('page_id', 'page_namespace', 'page_title', 'page_restrictions')
     reader = csv.DictReader(page_file, fieldnames = fieldnames, delimiter='\t')
     page_dict = [row for row in reader]
     
 
 with open('revision.csv', "rb") as revision_file:
-#with open('revision.csv', newline = '' , encoding = 'utf-8') as revision_file:			#Py 3
     fieldnames = ('rev_id', 'rev_page', 'rev_user', 'rev_timestamp', 'rev_len', 'rev_parent_id',
                   'rev_is_redirect', 'rev_minor_edit', 'rev_fa', 'rev_flist', 'rev_ga', 'rev_comment')
     reader = csv.DictReader(revision_file, fieldnames = fieldnames, delimiter='\t')
@@ -30,7 +27,6 @@ with open('revision.csv', "rb") as revision_file:
         revision_dict.append(row)
 
 with open('revision_hash.csv', "rb") as revision_hash_file:
-#with open('revision_hash.csv', newline = '' , encoding = 'utf-8') as revision_hash_file:		#Py 3
     fieldnames = ('rev_id', 'rev_page', 'rev_user', 'rev_hash')
     reader = csv.DictReader(revision_hash_file, fieldnames = fieldnames, delimiter='\t')
     revision_hash_dict = [row for row in reader]
@@ -56,11 +52,10 @@ for x in range(int(sys.argv[1])):
     total.append(run_time_test())
     print ('Run', x, 'took', total[x], 'secs')
     
-print ('Average running time:', sum(total)/len(total))
-print ('The fastest run was:', min(total), 'secs')
+print 'Average running time:', sum(total)/len(total)
+print 'The fastest run was:', min(total), 'secs'
 
-#print ('Average running time:', statistics.mean(total), 'secs')		#Py 3
-#print ('Standard Deviation:', statistics.stdev(total), 'secs')			#Py 3
+
 
 
 
---
+++ wikidat/sources/db/SQLAlchemy_files/testload_core_postgres3.py
@@ -0,0 +1,70 @@
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import statistics        
+import psycopg2
+import csv
+import sys
+import dbschema_core_postgresql
+import timeit
+
+
+engine = create_engine('postgresql+psycopg2://postgres:root@localhost/wikidb')
+
+
+with open('page.csv', newline = '' , encoding = 'utf-8') as page_file:			
+    fieldnames = ('page_id', 'page_namespace', 'page_title', 'page_restrictions')
+    reader = csv.DictReader(page_file, fieldnames = fieldnames, delimiter='\t')
+    page_dict = []
+    for row in reader:
+        row['page_restrictions'] =  row['page_restrictions'].encode('utf-8')
+        page_dict.append(row) 
+    
+with open('revision.csv', newline = '' , encoding = 'utf-8') as revision_file:			#Py 3
+    fieldnames = ('rev_id', 'rev_page', 'rev_user', 'rev_timestamp', 'rev_len', 'rev_parent_id',
+                  'rev_is_redirect', 'rev_minor_edit', 'rev_fa', 'rev_flist', 'rev_ga', 'rev_comment')
+    reader = csv.DictReader(revision_file, fieldnames = fieldnames, delimiter='\t')
+    revision_dict = []
+    for row in reader:
+        if row['rev_parent_id'] == 'NULL':
+            row['rev_parent_id'] = None 
+        revision_dict.append(row)
+
+with open('revision_hash.csv', newline = '' , encoding = 'utf-8') as revision_hash_file:		#Py 3
+    fieldnames = ('rev_id', 'rev_page', 'rev_user', 'rev_hash')
+    reader = csv.DictReader(revision_hash_file, fieldnames = fieldnames, delimiter='\t')
+    revision_hash_dict = []
+    for row in reader:
+        row['rev_hash'] =  row['rev_hash'].encode('utf-8')
+        revision_hash_dict.append(row)
+
+def load():
+    engine.execute(dbschema_core_postgresql.page.insert(),page_dict)
+	
+    engine.execute(dbschema_core_postgresql.revision.insert(),revision_dict)
+
+    engine.execute(dbschema_core_postgresql.revision_hash.insert(),revision_hash_dict)  
+    
+
+timer = timeit.Timer(stmt='load()', setup='from __main__ import load')
+
+total = []
+
+def run_time_test():
+    dbschema_core_postgresql.metadata.drop_all(engine)
+    dbschema_core_postgresql.metadata.create_all(engine)    
+    return(timer.timeit(number=1))
+
+for x in range(int(sys.argv[1])):
+    total.append(run_time_test())
+    print ('Run', x, 'took', total[x], 'secs')
+    
+
+
+print ('Average running time:', sum(total)/len(total))					
+print ('The fastest run was:', min(total), 'secs')					
+
+print ('Average running time:', statistics.mean(total), 'secs')		
+#print ('Standard Deviation:', statistics.stdev(total), 'secs')		
+
+
