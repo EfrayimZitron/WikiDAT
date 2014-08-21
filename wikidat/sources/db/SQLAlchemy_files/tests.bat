b3531720d2ef245554f7a5479f30007685339e76
Efrayim Zitron <efrayim.zitron@nyu.edu>
2014-08-21 14:50:00 -0400

    adding test results

--- wikidat/sources/db/SQLAlchemy_files/tests.bat
+++ wikidat/sources/db/SQLAlchemy_files/tests.bat
@@ -14,4 +14,8 @@ ECHO Python 2.7 + psycopg + SQLAlchemy Core >> results.txt
 C:\Python27\python2.exe C:\Users\Efi\Documents\GitHub\WikiDAT\wikidat\sources\db\SQLAlchemy_files\testload_core_postgres2.py 100 >> results.txt
 ECHO Python 3.4 + psycopg + SQLAlchemy Core >> results.txt
 C:\Python34\python.exe C:\Users\Efi\Documents\GitHub\WikiDAT\wikidat\sources\db\SQLAlchemy_files\testload_core_postgres3.py 100 >> results.txt
+ECHO Python 3.4 + psycopg + SQLAlchemy ORM >> results.txt
+C:\Python34\python.exe C:\Users\Efi\Documents\GitHub\WikiDAT\wikidat\sources\db\SQLAlchemy_files\testload_orm_postgres3.py 100 >> results.txt
+ECHO Python 2.7 + psycopg + SQLAlchemy ORM >> results.txt
+C:\Python27\python2.exe C:\Users\Efi\Documents\GitHub\WikiDAT\wikidat\sources\db\SQLAlchemy_files\testload_orm_postgres2.py 100 >> results.txt
 PAUSE
