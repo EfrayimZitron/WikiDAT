9290aaa656354a5a0bc55325041cd06cfa92fe1f
Efrayim Zitron <efrayim.zitron@nyu.edu>
2014-08-22 12:51:51 -0400

    Attempting to create functions that can be called
    The goal is to replace what the script as a whole has been doing with
    functions that can be called from outside

--- wikidat/sources/db/SQLAlchemy_files/dbschema_mysqldb_core.py
+++ wikidat/sources/db/SQLAlchemy_files/dbschema_mysqldb_core.py
@@ -12,102 +12,118 @@ from sqlalchemy import create_engine
 import MySQLdb
 import sys
 
-metadata = MetaData()
-
-dbengine = 'InnoDB'  # sys.argv[1]
-
-page = Table('page', metadata,
-    Column('page_id', INTEGER(unsigned=True), nullable=False),
-    Column('page_namespace', SMALLINT, nullable = False),
-    Column('page_title', VARCHAR(length=255, binary=True), nullable=False),
-    Column('page_restrictions', TINYBLOB, nullable=False),
-    mysql_engine= dbengine)
 
 
-revision = Table('revision', metadata,
-    Column('rev_id', INTEGER(unsigned=True), nullable=False),
-    Column('rev_page', INTEGER(unsigned=True), nullable=False),
-    Column('rev_user', INTEGER(unsigned=True), nullable=False, default=0),
-    Column('rev_timestamp', DATETIME, nullable=False),
-    Column('rev_len', INTEGER(unsigned=True), nullable=False),
-    Column('rev_parent_id', INTEGER(unsigned=True), nullable=True, default = 'NULL'),
-    Column('rev_is_redirect', TINYINT(display_width=1, unsigned=True), nullable=False, default=0),
-    Column('rev_minor_edit', TINYINT(display_width=1, unsigned=True), nullable=False, default=0),
-    Column('rev_fa', TINYINT(display_width=1, unsigned=True), nullable=False, default=0),
-    Column('rev_flist', TINYINT(display_width=1, unsigned=True), nullable=False, default=0),
-    Column('rev_ga', TINYINT(display_width=1, unsigned=True), nullable=False, default=0),
-    Column('rev_comment', TEXT, nullable=False),
-    mysql_engine= dbengine)
 
 
-revision_hash = Table('revision_hash', metadata,
-    Column('rev_id', INTEGER(unsigned=True), nullable=False),
-    Column('rev_page', INTEGER(unsigned=True), nullable=False),
-    Column('rev_user', INTEGER(unsigned=True), nullable=False, default=0),
-    Column('rev_hash', VARBINARY(256), nullable=False),
-    mysql_engine= dbengine)
 
 
-namespaces = Table('namespaces', metadata,
-    Column('code', SMALLINT, nullable=False),
-    Column('name', VARCHAR(50), nullable=False),
-    mysql_engine= dbengine)
 
+def define_tables(dbengine):
+    """
+	Defines the tables used in the WIkiDAT database
+	"""
+
+    metadata = MetaData()
+
+    page = Table('page', metadata,
+        Column('page_id', INTEGER(unsigned=True), nullable=False),
+        Column('page_namespace', SMALLINT, nullable = False),
+        Column('page_title', VARCHAR(length=255, binary=True), nullable=False),
+        Column('page_restrictions', TINYBLOB, nullable=False),
+        mysql_engine= dbengine)
+
+
+    revision = Table('revision', metadata,
+        Column('rev_id', INTEGER(unsigned=True), nullable=False),
+        Column('rev_page', INTEGER(unsigned=True), nullable=False),
+        Column('rev_user', INTEGER(unsigned=True), nullable=False, default=0),
+        Column('rev_timestamp', DATETIME, nullable=False),
+        Column('rev_len', INTEGER(unsigned=True), nullable=False),
+        Column('rev_parent_id', INTEGER(unsigned=True), nullable=True, default = 'NULL'),
+        Column('rev_is_redirect', TINYINT(display_width=1, unsigned=True), nullable=False, default=0),
+        Column('rev_minor_edit', TINYINT(display_width=1, unsigned=True), nullable=False, default=0),
+        Column('rev_fa', TINYINT(display_width=1, unsigned=True), nullable=False, default=0),
+        Column('rev_flist', TINYINT(display_width=1, unsigned=True), nullable=False, default=0),
+        Column('rev_ga', TINYINT(display_width=1, unsigned=True), nullable=False, default=0),
+        Column('rev_comment', TEXT, nullable=False),
+        mysql_engine= dbengine)
+
+
+    revision_hash = Table('revision_hash', metadata,
+        Column('rev_id', INTEGER(unsigned=True), nullable=False),
+        Column('rev_page', INTEGER(unsigned=True), nullable=False),
+        Column('rev_user', INTEGER(unsigned=True), nullable=False, default=0),
+        Column('rev_hash', VARBINARY(256), nullable=False),
+        mysql_engine= dbengine)
+
+
+    namespaces = Table('namespaces', metadata,
+        Column('code', SMALLINT, nullable=False),
+        Column('name', VARCHAR(50), nullable=False),
+        mysql_engine= dbengine)
+
+        
+    people = Table('people', metadata,
+        Column('rev_user', INTEGER(unsigned=True), nullable=False, default=0),
+        Column('rev_user_text', VARCHAR(length=255, binary=True), nullable=True, default=''),
+        mysql_engine= dbengine)
+
+
+    logging = Table('logging', metadata,
+        Column('log_id', INTEGER(unsigned=True), nullable=False),
+        Column('log_type', VARCHAR(length=15, binary=True), nullable=False),
+        Column('log_action', VARCHAR(length=15, binary=True), nullable=False),
+        Column('log_timestamp', DATETIME, nullable=False),
+        Column('log_user', INTEGER(unsigned=True), nullable=False),
+        Column('log_username', VARCHAR(length=255, binary=True), nullable=False, default=''),
+        Column('log_namespace', INTEGER(display_width=5), nullable=False, default=0),
+        Column('log_title', VARCHAR(length=255, binary=True), nullable=False, default=''),
+        Column('log_comment', VARCHAR(length=255, binary=True), nullable=False, default=''),
+        Column('log_params', VARCHAR(length=255, binary=True), nullable=False, default=''),
+        Column('log_new_flag', INTEGER(unsigned=True), nullable=False, default=0),
+        Column('log_old_flag', INTEGER(unsigned=True), nullable=False),
+        mysql_engine= dbengine)
+
+    block = Table('block', metadata,
+        Column('block_id', INTEGER(unsigned=True), nullable=False),
+        Column('block_action', VARCHAR(length = 15, binary = True), nullable=False),
+        Column('block_user', INTEGER(unsigned=True), nullable=False),
+        Column('block_timestamp', DATETIME, nullable=False),
+        Column('block_target', INTEGER, nullable=False),
+        Column('block_ip', INTEGER(display_width= 10, unsigned=True), nullable= False),
+        Column('block_duration', INTEGER(unsigned=True), nullable=False),
+        mysql_engine= dbengine)
+
+        
+    new_user = Table('new_user', metadata,
+        Column('user_id',INTEGER(unsigned=True), nullable=False),
+        Column('username', VARCHAR(length = 255), nullable=False),
+        Column('user_timestamp', DATETIME, nullable=False),
+        Column('user_action', VARCHAR(15), nullable=False),
+        mysql_engine= dbengine)
+
+        
+    right = Table('right', metadata,
+        Column('right_id', INTEGER(unsigned=True), nullable=False),
+        Column('right_username', VARCHAR(length = 255), nullable=False),
+        Column('right_timestamp', DATETIME, nullable=False),
+        Column('right_old', VARCHAR(length = 255), nullable=False),
+        Column('right_new', VARCHAR(length = 255), nullable=False),
+        mysql_engine= dbengine)
 	
-people = Table('people', metadata,
-    Column('rev_user', INTEGER(unsigned=True), nullable=False, default=0),
-    Column('rev_user_text', VARCHAR(length=255, binary=True), nullable=True, default=''),
-    mysql_engine= dbengine)
-
-
-logging = Table('logging', metadata,
-    Column('log_id', INTEGER(unsigned=True), nullable=False),
-    Column('log_type', VARCHAR(length=15, binary=True), nullable=False),
-    Column('log_action', VARCHAR(length=15, binary=True), nullable=False),
-    Column('log_timestamp', DATETIME, nullable=False),
-    Column('log_user', INTEGER(unsigned=True), nullable=False),
-    Column('log_username', VARCHAR(length=255, binary=True), nullable=False, default=''),
-    Column('log_namespace', INTEGER(display_width=5), nullable=False, default=0),
-    Column('log_title', VARCHAR(length=255, binary=True), nullable=False, default=''),
-    Column('log_comment', VARCHAR(length=255, binary=True), nullable=False, default=''),
-    Column('log_params', VARCHAR(length=255, binary=True), nullable=False, default=''),
-    Column('log_new_flag', INTEGER(unsigned=True), nullable=False, default=0),
-    Column('log_old_flag', INTEGER(unsigned=True), nullable=False),
-    mysql_engine= dbengine)
-
-block = Table('block', metadata,
-    Column('block_id', INTEGER(unsigned=True), nullable=False),
-    Column('block_action', VARCHAR(length = 15, binary = True), nullable=False),
-    Column('block_user', INTEGER(unsigned=True), nullable=False),
-    Column('block_timestamp', DATETIME, nullable=False),
-    Column('block_target', INTEGER, nullable=False),
-    Column('block_ip', INTEGER(display_width= 10, unsigned=True), nullable= False),
-    Column('block_duration', INTEGER(unsigned=True), nullable=False),
-    mysql_engine= dbengine)
-
-	
-new_user = Table('new_user', metadata,
-    Column('user_id',INTEGER(unsigned=True), nullable=False),
-    Column('username', VARCHAR(length = 255), nullable=False),
-    Column('user_timestamp', DATETIME, nullable=False),
-    Column('user_action', VARCHAR(15), nullable=False),
-    mysql_engine= dbengine)
-
-	
-right = Table('right', metadata,
-    Column('right_id', INTEGER(unsigned=True), nullable=False),
-    Column('right_username', VARCHAR(length = 255), nullable=False),
-    Column('right_timestamp', DATETIME, nullable=False),
-    Column('right_old', VARCHAR(length = 255), nullable=False),
-    Column('right_new', VARCHAR(length = 255), nullable=False),
-    mysql_engine= dbengine)
-	
-	
-engine = create_engine('mysql+mysqldb://root:@localhost/')
-engine.execute("DROP DATABASE IF EXISTS wikidb")
-engine.execute("CREATE DATABASE IF NOT EXISTS wikidb CHARACTER SET utf8 COLLATE utf8_general_ci")  # create db
-engine.execute("USE wikidb")
-metadata.create_all(engine)
+    return metadata
+    
     
 
-
+    
+def create_database(dbengine):
+    """
+	Creates the wikidb database. A storage engine is passed as an argument
+	"""
+    metadata = define_tables(dbengine)
+    engine = create_engine('mysql+mysqldb://root:@localhost/')
+    engine.execute("DROP DATABASE IF EXISTS wikidb")
+    engine.execute("CREATE DATABASE IF NOT EXISTS wikidb CHARACTER SET utf8 COLLATE utf8_general_ci")  # create db
+    engine.execute("USE wikidb")
+    metadata.create_all(engine)
--- wikidat/sources/db/SQLAlchemy_files/testload_mysqldb_core.py
+++ wikidat/sources/db/SQLAlchemy_files/testload_mysqldb_core.py
@@ -1,5 +1,5 @@
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 import MySQLdb
 import csv
@@ -8,51 +8,68 @@ import dbschema_mysqldb_core
 import timeit
 
 engine = create_engine('mysql+mysqldb://root:@localhost/wikidb')
+meta = MetaData()
+meta.reflect(bind=engine)
+page = meta.tables['page']
+revision =  meta.tables['revision']
+revision_hash = meta.tables['revision_hash']
 
-with open('page.csv') as page_file:
-    fieldnames = ('page_id', 'page_namepsace', 'page_title', 'page_restrictions')
-    reader = csv.DictReader(page_file, fieldnames = fieldnames, delimiter='\t')
-    page_dict = [row for row in reader]
 
-with open('revision.csv') as revision_file:
-    fieldnames = ('rev_id', 'rev_page', 'rev_user', 'rev_timestamp', 'rev_len', 'rev_parent_id',
-                  'rev_is_redirect', 'rev_minor_edit', 'rev_fa', 'rev_flist', 'rev_ga', 'rev_comment')
-    reader = csv.DictReader(revision_file, fieldnames = fieldnames, delimiter='\t')
-    revision_dict = []
-    for row in reader:
-        if row['rev_parent_id'] == 'NULL':
-            row['rev_parent_id'] = None 
-        revision_dict.append(row)
-    
+def read_csv():
+	"""
+	Reads CSV files (for page, revision, and revision_hash) into data structure
+	"""
+	with open('page.csv') as page_file:
+		fieldnames = ('page_id', 'page_namepsace', 'page_title', 'page_restrictions')
+		reader = csv.DictReader(page_file, fieldnames = fieldnames, delimiter='\t')
+		global page_dict
+		page_dict = [row for row in reader]
 
-with open('revision_hash.csv') as revision_hash_file:
-    fieldnames = ('rev_id', 'rev_page', 'rev_user', 'rev_hash')
-    reader = csv.DictReader(revision_hash_file, fieldnames = fieldnames, delimiter='\t')
-    revision_hash_dict = [row for row in reader]
+	with open('revision.csv') as revision_file:
+		fieldnames = ('rev_id', 'rev_page', 'rev_user', 'rev_timestamp', 'rev_len', 'rev_parent_id',
+		'rev_is_redirect', 'rev_minor_edit', 'rev_fa', 'rev_flist', 'rev_ga', 'rev_comment')
+		reader = csv.DictReader(revision_file, fieldnames = fieldnames, delimiter='\t')
+		global revision_dict
+		revision_dict = []
+		for row in reader:
+			if row['rev_parent_id'] == 'NULL':
+				row['rev_parent_id'] = None 
+			revision_dict.append(row)
+
+	with open('revision_hash.csv') as revision_hash_file:
+		fieldnames = ('rev_id', 'rev_page', 'rev_user', 'rev_hash')
+		reader = csv.DictReader(revision_hash_file, fieldnames = fieldnames, delimiter='\t')
+		global revision_hash_dict
+		revision_hash_dict = [row for row in reader]
 
 
 def load():
-    engine.execute(dbschema_mysqldb_core.page.insert(),page_dict)
+	"""
+	Performs the actual insertions into the database
+	"""
+	engine.execute(page.insert(),page_dict)
 
-    engine.execute(dbschema_mysqldb_core.revision.insert(),revision_dict)
+	engine.execute(revision.insert(),revision_dict)
 
-    engine.execute(dbschema_mysqldb_core.revision_hash.insert(),revision_hash_dict) 
+	engine.execute(revision_hash.insert(),revision_hash_dict) 
+	
 
-# Timer calculates how much time it takes for the data to be loaded into the database.
-# Had to specify that the timer should only run one execution of the code since multiple runs of inserting identical
-# data would fail due to primary key violations
-timer = timeit.Timer(stmt='load()', setup='from __main__ import load')
-
-total = []
+timer = timeit.Timer(stmt='load()', setup='from testload_mysqldb_core import load')	
 
 def run_time_test():
-    dbschema_mysqldb_core.metadata.drop_all(engine)
-    dbschema_mysqldb_core.metadata.create_all(engine)   
-    return(timer.timeit(number=1))
-    
-for x in range(int(sys.argv[1])):
-    total.append(run_time_test())
-    print 'Run', x, 'took', total[x]
+	"""
+	Deletes and recreates the database, and performs one run of inserts
+	"""
+	meta.drop_all(engine)
+	meta.create_all(engine)   
+	return(timer.timeit(number=1))
 
-print 'The fastest run was:', min(total), 'secs'    
-print 'Average running time:', sum(total)/len(total)
+def bulk_insert():
+	read_csv()
+	total = []
+	for x in range(1):
+		total.append(run_time_test())
+		print 'Run', x, 'took', total[x]
+
+	print 'The fastest run was:', min(total), 'secs'    
+	print 'Average running time:', sum(total)/len(total)
